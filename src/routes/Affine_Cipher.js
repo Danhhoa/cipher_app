c9ec97b90b1cdddaaedca56a0d24d1603be2a1cf
Dinh LE Minh Bach <you@example.com>
2022-04-15 12:28:57 +0700

    hill and rail fence

--- src/routes/Affine_Cipher.js
+++ src/routes/Affine_Cipher.js
@@ -34,7 +34,7 @@ const Affine = () => {
                 }
                 return letters[(a * letters.indexOf(x) + b) % letters.length]
                 })
-            .flatMap((x, i, xs) => (i + 1 < xs.length) && (i + 1) % 1 === 0 ? [x, " "] : [x])
+            .flatMap((x, i, xs) => (i + 1 < xs.length) && (i + 1) % 1 === 0 ? [x, ""] : [x])
             .join(" ")
         };
         
@@ -54,7 +54,7 @@ const Affine = () => {
                 const z = gcd(a, letters.length) * (letters.indexOf(x) - b)
                 return letters[mod(z, letters.length)]
             })
-            .flatMap((x, i, xs) => (i + 1 < xs.length) && (i + 1) % 1 === 0 ? [x, " "] : [x])
+            .flatMap((x, i, xs) => (i + 1 < xs.length) && (i + 1) % 1 === 0 ? [x, ""] : [x])
             .join(" ")
     };
 
@@ -62,10 +62,6 @@ const Affine = () => {
     const [cipherText, setCipherText] = useState('')
     const [keyA, setKeyA] = useState(1)
     const [keyB, setKeyB] = useState(1)
-
-
-    let encrypt = encode(plainText, {a:keyA, b:keyB})
-    let decrypt = decode(encrypt, {a:keyA, b:keyB})
 
     console.log(plainText) 
     console.log(cipherText)
@@ -76,6 +72,7 @@ const Affine = () => {
             <h1> thuật toán Affine</h1>
             <div className="container">
                 <div className="plain__content">
+                <label className='label-title'>Encrypt</label>
                     <textarea className='text' type="text" onChange={e => setPlainText(e.target.value)} value={plainText}>
                         {plainText}
                     </textarea>
@@ -84,27 +81,28 @@ const Affine = () => {
                     <div className="field field-number field--first">
                         <label className='label'>slope / a</label>
                         <div className="field-number__field">
-                            <button className='field-number__down' onClick={() => setKeyA(keyA - 2 === 13 ? keyA - 4 : keyA - 2)}>-</button>
+                            <button className='field-number__down' onClick={() => setKeyA(keyA => keyA - 2 === 13 ? keyA - 4 : keyA - 2)}>-</button>
                             <div className="field-number__value">{keyA}</div>
-                            <button className='field-number__up' onClick={() => setKeyA(keyA + 2 === 13 ? keyA + 4 : keyA + 2)}>+</button>
+                            <button className='field-number__up' onClick={() => setKeyA(keyA => keyA + 2 === 13 ? keyA + 4 : keyA + 2)}>+</button>
                         </div>
                     </div>
                     
                     <div className="field field-number field--second">
                         <label className='label'>intercept / b</label>
                         <div className="field-number__field">
-                            <button className='field-number__down' onClick={() => setKeyB(keyB - 1)}>-</button>
+                            <button className='field-number__down' onClick={() => setKeyB(keyB => keyB - 1)}>-</button>
                             <div className="field-number__value">{keyB}</div>
-                            <button className='field-number__up' onClick={() => setKeyB(keyB + 1)}>+</button>
+                            <button className='field-number__up' onClick={() => setKeyB(keyB => keyB + 1)}>+</button>
                         </div>
                     </div>
                     
                     <div className="btn-container">
-                            <button onClick={() => setCipherText(encrypt)}>Encrypt</button>
-                            <button onClick={() => setPlainText(decrypt)}>Decrypt</button>
+                            <button onClick={() => setCipherText(encode(plainText, {a:keyA, b:keyB}))}>Encrypt</button>
+                            <button onClick={() => setPlainText(decode(cipherText, {a:keyA, b:keyB}))}>Decrypt</button>
                     </div>
                 </div>
                 <div className="cipher__content">
+                    <label className='label-title'>Decrypt</label>
                     <textarea className='text' type="text" onChange={e => setCipherText(e.target.value)} value={cipherText}>
                         {cipherText}
                     </textarea>
--- src/routes/Transposition_Cipher.js
+++ src/routes/Transposition_Cipher.js
@@ -1,7 +1,123 @@
+import { useState, useEffect } from 'react'
+
 const Transposition = () => {
+
+    // This is only a SKELETON file for the 'Rail Fence Cipher' exercise. It's been provided as a
+    // convenience to get you started writing code faster.
+    //
+    const encode = (input, rails) => {
+        if (!input) return input
+        const arr = input.split('')
+        let res = [...Array(rails)].map(i => [])
+        let i = 0
+        let direction = 'down'
+        while (arr.length) {
+            res[i].push(arr[0])
+            arr.shift()
+            if (i >= rails - 1) direction = 'up'
+            else if (i === 0) direction = 'down'
+            i = direction === 'down' ? i + 1 : i - 1
+        }
+
+        res = res.reduce((prev, curr) => prev.concat(curr)).join('')
+            return res
+    }
+    
+    const decode = (input, rails) => {
+        if (!input) return input
+        const arr = input.split('')
+        let res = [...Array(rails)].map(i => [])
+        let row = 0
+        let col = 0
+        // create the matrix
+        while (row < rails) {
+            res[row].push('-')
+            col++
+            if (col === arr.length) {
+            row++
+            col = 0
+            }
+        }
+        // insert placeholders
+        let direction = 'down'
+        row = 0
+        col = 0
+        while (col < arr.length) {
+            res[row][col] = '*'
+            col++
+            if (row >= rails - 1) direction = 'up'
+            else if (row === 0) direction = 'down'
+            row = direction === 'down' ? row + 1 : row - 1
+        }
+        // replace placeholders for letters
+        row = 0
+        col = 0
+        while (arr.length) {
+            if (res[row][col] === '*') {
+            res[row][col] = arr[0]
+            arr.shift()
+            }
+            col++
+            if (col === res[row].length) {
+            col = 0
+            row++
+            }
+        }
+        // read the message
+        const realRes = []
+        row = 0
+        col = 0
+        direction = 'down'
+        while (col < res[0].length) {
+            realRes.push(res[row][col])
+            col++
+            if (row >= rails - 1) direction = 'up'
+            else if (row === 0) direction = 'down'
+            row = direction === 'down' ? row + 1 : row - 1
+        }
+        return realRes.join('')
+    }
+
+    const [plainText, setPlainText] = useState('')
+    const [cipherText, setCipherText] = useState('')
+
+    const [key, setKey] = useState(1)
+
+    console.log(decode('GeeksforGeeks', 4))
+    
+    console.log(key)
+
+
+
     return (
         <div className="right-col">
-        <h1> thuật toán Transposition</h1>
+            <h1> thuật toán Rail Fence - Transposition</h1>
+            <div className="container">
+                <div className="plain__content">
+                    <textarea className='text' type="text" onChange={e => setPlainText(e.target.value)} value={plainText}>
+                        {plainText}
+                    </textarea>
+                </div>
+                <div className="convert__content transposition">
+                    <div className="field field-number field--first field-transposition">
+                        <label className='label'>KEY</label>
+                        <div className="field-number__field">
+                            <button className='field-number__down' onClick={() => setKey(key => key - 1)}>-</button>
+                            <div className="field-number__value">{key}</div>
+                            <button className='field-number__up' onClick={() => setKey(key => key + 1)}>+</button>
+                        </div>
+                    </div>   
+                    <div className="btn-container btn-transposition">
+                            <button onClick={() => setCipherText(encode(plainText, key))}>Encrypt</button>
+                            <button onClick={() => setPlainText(decode(cipherText, key))}>Decrypt</button>
+                    </div>
+                </div>
+                <div className="cipher__content">
+                    <textarea className='text' type="text" onChange={e => setCipherText(e.target.value)} value={cipherText}>
+                        {cipherText}
+                    </textarea>
+                </div>
+            </div>
         </div>
     )
 }
